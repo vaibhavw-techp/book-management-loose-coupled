5ffd1a9181cfbcc44288c502c714ed435b6fb815
vaibhav <vaibhav.waghmare@techprescient.com>
2024-02-22 10:57:29 +0530

    Added Entities Author And Book

---
+++ src/main/java/com/bookmanagement/BookManagement/entity/Author.java
@@ -0,0 +1,24 @@
+package com.bookmanagement.BookManagement.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+}
