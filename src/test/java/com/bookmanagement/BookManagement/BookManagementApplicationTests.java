c9d2f4cb3329841d1bea17a8dd4a6f229a1f668b
vaibhav <vaibhav.waghmare@techprescient.com>
2024-02-22 10:55:47 +0530

    Initial Commit

---
+++ src/test/java/com/bookmanagement/BookManagement/BookManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmanagement.BookManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
