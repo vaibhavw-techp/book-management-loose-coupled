d67f2f5705dd861c0361798ab4334333d9fed84c
vaibhav <vaibhav.waghmare@techprescient.com>
2024-02-22 10:59:32 +0530

    Added Mapstruct Mapper interfaces for Author and Book

---
+++ src/main/java/com/bookmanagement/BookManagement/mapper/AuthorMapper.java
@@ -0,0 +1,28 @@
+package com.bookmanagement.BookManagement.mapper;
+
+import com.bookmanagement.BookManagement.dto.AuthorDto;
+import com.bookmanagement.BookManagement.entity.Author;
+import com.bookmanagement.BookManagement.entity.Book;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring", uses = BookMapper.class)
+public interface AuthorMapper {
+
+    AuthorDto toDTO(Author author);
+
+    Author toEntity(AuthorDto authorDto);
+
+    @Mapping(target = "author.name", source = "authorDto.name")
+//    @Mapping(target = "author.books", source = "authorDto.books")
+    void updateAuthor(AuthorDto authorDto, @MappingTarget Author author);
+
+    @AfterMapping
+    public default void AfterMap(AuthorDto authorDto, @MappingTarget Author author){
+        for(Book book: author.getBooks()) {
+            book.setAuthor(author);
+        }
+    }
+}
---
+++ src/main/java/com/bookmanagement/BookManagement/mapper/BookMapper.java
@@ -0,0 +1,28 @@
+package com.bookmanagement.BookManagement.mapper;
+
+import com.bookmanagement.BookManagement.dto.BookDto;
+import com.bookmanagement.BookManagement.entity.Book;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface BookMapper {
+
+    @Mapping(target = "title", source = "title")
+    @Mapping(target = "price", source = "price")
+    @Mapping(target = "publication", source = "publication")
+    BookDto toDTO(Book book);
+
+
+    @Mapping(target = "title", source = "title")
+    @Mapping(target = "price", source = "price")
+    @Mapping(target = "publication", source = "publication")
+    Book toEntity(BookDto dto);
+
+
+    List<Book> getEntityList(List<BookDto> bookDtos);
+
+
+}
